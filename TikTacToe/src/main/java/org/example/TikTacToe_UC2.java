89f09a90fd7a1368acdc2d0c0c35398ff8191f2d
lc2420 <lc2420@srmist.edu.in>
2026-04-21 10:16:11 +0530

    Adding UC2 code

---
+++ TikTacToe/src/main/java/org/example/TikTacToe_UC2.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class TicTacToe_UC2 {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+
+        if (random.nextBoolean()) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and will play first.");
+        } else {
+            System.out.println("Computer won the toss and will play first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+}
