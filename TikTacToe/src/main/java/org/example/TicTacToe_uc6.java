230b1a0c59b9ea2c915fb111c671e3c0fef0fbc6
lc2420 <lc2420@srmist.edu.in>
2026-04-29 09:11:45 +0530

    Adding the file

---
+++ TikTacToe/src/main/java/org/example/TicTacToe_uc6.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class TicTacToe_uc6 {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
+    }
+
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+}
