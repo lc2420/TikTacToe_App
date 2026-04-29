dc965de9c28387747ae389516841e9fefbae054f
lc2420 <lc2420@srmist.edu.in>
2026-04-29 09:06:22 +0530

    Adding the file

---
+++ TikTacToe/src/main/java/org/example/UC5.java
@@ -0,0 +1,19 @@
+public class TicTacToe_uc5 {
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isValidMove(1, 1));
+    }
+
+    static boolean isValidMove(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+            return board[row][col] == '-';
+        }
+        return false;
+    }
+}
